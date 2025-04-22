95344d2e18c72918b8b82ff1da3482e66f7388e8
William Pittman <wspittman@gmail.com>
2025-04-22 09:47:06 -0700

    gemini package skeleton

---
+++ packages/gemini/src/index.ts
@@ -0,0 +1,9 @@
+import { externalLog, type LogOptions } from "./externalLog.ts";
+
+//export { z } from "zod";
+//export { jsonCompletion } from "./gemini.ts";
+//export { zBoolean, zEnum, zNumber, zObj, zObjArray, zString } from "./zod.ts";
+
+export function setAILogging(options: LogOptions): void {
+  externalLog.setFn("Gemini", options);
+}
